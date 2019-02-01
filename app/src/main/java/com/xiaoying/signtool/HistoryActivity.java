1f027291f2edadd8fb9d99f65c40214c0224d5fa
zhangyy <zhangyy_owen@139.com>
2019-02-01 10:52:31 +0800

    添加历史记录清除功能

--- app/src/main/java/com/xiaoying/signtool/HistoryActivity.java
+++ app/src/main/java/com/xiaoying/signtool/HistoryActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -23,10 +24,15 @@ import java.util.Map;
 
 public class HistoryActivity extends AppCompatActivity {
 
+    private SharedPreferences mSP;
+    private  ArrayAdapter<String> mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        mSP = getSharedPreferences("app_data", Context.MODE_PRIVATE);
 
         initView();
     }
@@ -37,11 +43,40 @@ public class HistoryActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.history_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 exitWithResult(RESULT_CANCELED, null); // back button
                 return true;
+            case R.id.action_history_clear:
+                new AlertDialog.Builder(HistoryActivity.this)
+                        .setMessage(R.string.clear_hsitory)
+                        .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mSP.edit().clear().apply();
+                                if(null != mAdapter) {
+                                    mAdapter.clear();
+                                    mAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
+                        .show();
+                return true;
+            default:
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -57,9 +92,8 @@ public class HistoryActivity extends AppCompatActivity {
 
         ListView lvList = findViewById(R.id.lv_history);
 
-        final SharedPreferences sp = getSharedPreferences("app_data", Context.MODE_PRIVATE);
         @SuppressWarnings("unchecked")
-        final Map<String, Long> historyData = (Map<String, Long>) sp.getAll();
+        final Map<String, Long> historyData = (Map<String, Long>) mSP.getAll();
         final List<Map.Entry<String, Long>> entryList = new ArrayList<>(historyData.entrySet());
         // sort
         Collections.sort(entryList, new ValueComparator());
@@ -68,8 +102,8 @@ public class HistoryActivity extends AppCompatActivity {
             packageNames.add(entry.getKey());
         }
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, packageNames);
-        lvList.setAdapter(adapter);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, packageNames);
+        lvList.setAdapter(mAdapter);
 
         lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -86,9 +120,9 @@ public class HistoryActivity extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 if(0 == which) {
-                                    sp.edit().remove(packageNames.get(position)).apply();
+                                    mSP.edit().remove(packageNames.get(position)).apply();
                                     packageNames.remove(position);
-                                    adapter.notifyDataSetChanged();
+                                    mAdapter.notifyDataSetChanged();
                                 }
                             }
                         })
